c4e449f01a4bc657d105ae7e125ba5c8b5dc9352
Don Kackman <dkackman@gmail.com>
2023-11-16 21:25:56 -0600

    fix service install location

--- install.ps1
+++ install.ps1
@@ -3,11 +3,18 @@
 
 Param(
     [Parameter(Mandatory=$false)]
-    [string]$BinPath = ".\standalone\win-x64\DlMirrorSync.exe"
+    [string]$BinPath = ".\publish\standalone\win-x64\DlMirrorSync.exe"
 )
+
+# Check if BinPath is absolute
+if (-not [System.IO.Path]::IsPathRooted($BinPath)) {
+    # If BinPath is not absolute, make it absolute
+    $BinPath = Join-Path -Path $PSScriptRoot -ChildPath $BinPath
+}
+$BinPath = Resolve-Path $BinPath
 
 $serviceName = "Data Layer Mirror Sync Service"
 $Description = "The Data Layer Mirror Sync Service watches for new chia data layer stores and subscribes to them."
 
-sc.exe create $serviceName binpath=$BinPath start=auto
+sc.exe create $serviceName start=auto binpath="$BinPath $env:USERPROFILE\.chia\mainnet\config\config.yaml"
 sc.exe description $serviceName $Description
